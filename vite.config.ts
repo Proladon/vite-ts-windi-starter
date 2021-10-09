fb299ea824a8c54e5becc2860e498802f3b5cd2b
Proladon <proladon@gmail.com>
2021-10-09 21:19:30 +0800

    add vite @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    WindiCSS(),
-  ]
+  base: '/',
+  plugins: [vue(), WindiCSS()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+  },
 })
